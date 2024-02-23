b7cb3976f44e1850245af2dd9791381e9fa8915e
Brian Arriaga <brian.arriaga@gmail.com>
2024-02-22 20:40:19 -0500

    Added introduction to Flask and decorators practice

---
+++ Practice/Decorators/main.py
@@ -0,0 +1,128 @@
+# # Functions inputs/functionality/outputs
+# def add(n1, n2):
+#     return n1 + n2
+
+
+# def subtract(n1, n2):
+#     return n1 - n2
+
+
+# def multiply(n1, n2):
+#     return n1 * n2
+
+
+# def divide(n1, n2):
+#     return n1 / n2
+
+
+# # Functions are first-class objects, can be passed around as arguments e.g. int/string/float etc.
+# def calculate(calc_function, n1, n2):
+#     return calc_function(n1, n2)
+
+
+# result = calculate(multiply, 2, 3)  # 6
+# print(result)
+
+
+# # Nested Functions
+# def outer_function():
+#     print("I'm the outer function")
+
+#     def nested_function():
+#         print("I'm the inner function")
+
+#     nested_function()
+
+
+# outer_function()
+
+
+# # Functions can be returned from other functions
+# def outer_function2():
+#     print("I'm the outer function 2")
+
+#     def nested_function2():
+#         print("I'm the inner function 2")
+
+#     return nested_function2
+
+
+# inner_function = outer_function2()
+# inner_function()
+
+
+# Python Decorators Function
+# import time
+
+
+# # def decorator_function(original_function):
+# #     def wrapper_function():
+# #         print(f"Wrapper executed this before {original_function.__name__} function")
+# #         original_function()
+
+# #     return wrapper_function
+
+
+# def delay_decorator(function):
+#     def wrapper_function():
+#         time.sleep(2)
+#         # Do something before
+#         function()
+#         function()
+#         # Do something after
+
+#     return wrapper_function
+
+
+# @delay_decorator
+# def say_hello():
+#     print("Hello")
+
+
+# @delay_decorator
+# def say_bye():
+#     print("Bye")
+
+
+# def say_greeting():
+#     print("How are you?")
+
+
+# # say_hello()
+# # say_greeting()
+
+# decorated_function = delay_decorator(say_greeting)
+# decorated_function()
+
+import time
+
+current_time = time.time()
+print(current_time)  # seconds since Jan 1st, 1970
+
+# Write your code below 👇
+
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        print(f"{function.__name__} run speed: {end_time - start_time}s")
+
+    return wrapper_function
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+
+fast_function()
+slow_function()
---
+++ Practice/Flask/README.md
@@ -0,0 +1,9 @@
+[Flask Quickstart](https://flask.palletsprojects.com/en/3.0.x/quickstart/)
+
+To run the application, use the `flask` command or `python -m flask`. You need to tell the Flask where your application is with the `--app` option.
+
+```bash
+$ flask --app hello run
+ * Serving Flask app 'hello'
+ * Running on http://127.0.0.1:5000 (Press CTRL+C to quit)
+```
